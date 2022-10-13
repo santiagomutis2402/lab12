e8c21915349c542aa17d5b206b8f9c58d2016465
Santiago Mutis <56487902+santiagomutis2402@users.noreply.github.com>
2022-10-12 20:39:37 -0500

    Update lab122.php

--- lab122.php
+++ lab122.php
@@ -18,7 +18,7 @@
         $var = $_SESSION['var'];
         print("<p>Valor de la variable de sesion: $var </p>\n");
         unset($_SESSION['var']);
-        print("<a href='lab23.php'>Paso 3</a>");
+        print("<a href='lab123.php'>Paso 3</a>");
     } else {
         print("Sesion no iniciada, ir al <a href='lab121.php'>paso 1<a> para iniciar la sesion");
     }
